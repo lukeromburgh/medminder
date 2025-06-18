a57d0973fd5fa75e9a751061d7a6e2224eef915a
lukeromburgh <i87v8t9s@students.codeinstitute.net>
2025-06-18 18:35:19 +0100

    Improve template layout

--- medminder_project/apps/documentation/views.py
+++ medminder_project/apps/documentation/views.py
@@ -9,10 +9,13 @@ from django.shortcuts import render
 
 def overview(request):
     context = {
-        'page_title': 'My Documentation',
-        'background_color': "#54bbff",  # Light blue
-        'left_title': 'apps.reminders',
-        'left_subtitle': 'A comprehensive reminder system for managing tasks and notifications.',
+        'page_title': 'Overview & Documentation',
+        'background_color': "#0F0F14",
+        'text_color': "#E2EAF3",
+        'left_title': 'Why I Built MedMinder',
+        'left_subtitle':'''During my time volunteering at an old age home, I saw firsthand the immense challenges many residents faced in managing their medication schedules. It was clear that staying on top of rigid, timely doses was a significant hurdle, often leading to missed medications or confusion. This experience sparked an idea: what if there was a way to make medication adherence simpler, more engaging, and less of a burden?
+
+That's why I created MedMinder, a medical reminder app designed to transform how people approach their medication routines. My primary goal was to address the critical need for a more effective and user-friendly solution for medication management, particularly for those with complex schedules.''',
         'right_title': 'Key features',
         'body_1': '<p>First section of content with detailed information about the feature set.</p>',
         'body_2': '<p>Second section covering additional functionality and capabilities.</p>'
