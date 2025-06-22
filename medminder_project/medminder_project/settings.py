b234ad9baa1cbb43320ec4c2fb4ae76039bb866b
lukeromburgh <i87v8t9s@students.codeinstitute.net>
2025-06-22 15:04:59 +0100

    Update STATIC_URL and configure STATIC_ROOT for static files

--- medminder_project/medminder_project/settings.py
+++ medminder_project/medminder_project/settings.py
@@ -147,13 +147,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
