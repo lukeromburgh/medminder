4aa66d7c3b72e96b2bf714325f95a2168d7fb332
Luke Romburgh <i87v8t9s@students.codeinstitute.net>
2025-06-29 16:12:33 +0100

    Turn debug off (#15)

--- medminder_project/medminder_project/settings.py
+++ medminder_project/medminder_project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)9m#&d*s0+rmpo2*@z7c3w1gr+lgv(vi5u_&q6xv(*eab*cdd^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
